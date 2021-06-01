8ac0d24c4f5a701c36ab276a0347753b6ca0a52b
advaitrane <advaitr11@gmail.com>
2021-06-02 00:26:41 +0530

    Added sigmoid, softmax, softmax base notebooks

--- code/ExperimentModules/embedding_models.py
+++ code/ExperimentModules/embedding_models.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 import torchvision.transforms as transforms
-import keras
+from tensorflow import keras
 
 # Data processing
 import PIL
